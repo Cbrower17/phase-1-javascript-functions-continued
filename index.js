0f174dd2375b5094bd81718f2871f552c24c333f
Christopher <chris.brower.96@gmail.com>
2023-01-31 08:10:46 -0700

    functional functions

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+
+function saturdayFun(sat = 'roller-skate') {
+    return 'This Saturday, I want to ' + sat + '!';
+}
+
+function mondayWork(mon = 'go to the office') { 
+    return 'This Monday, I will ' + mon + '.'; 
+}
+
+// function wrapAdjective(emph, adjective) {
+//     if(emp === '*'){
+//     return '*' + adjective + '*';
+//     } else {
+//         return  '||' + adjective + '||';
+//     }
+// }
+
+function wrapAdjective(emp = '*', msg = "You are ") {
+    const innerFunction = function ( adj = "special") {
+      return `${msg}${emp}${adj}${emp}!`;
+    };
+    return innerFunction;
+  }
